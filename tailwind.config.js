e0a049f59cd2e8c5b6bcac90d0700cc5176e5114
JBraddockm <jbraddockm@gmail.com>
2024-03-08 11:38:54 +0000

    Removed redundant animations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      animation: {
-        slideIn: 'slideIn 0.3s ease-out',
-        slideOut: 'slideOut 0.5s ease-out'
-      },
-      keyframes: {
-        slideIn: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0)' }
-        },
-        slideOut: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(100%)' }
-        }
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
